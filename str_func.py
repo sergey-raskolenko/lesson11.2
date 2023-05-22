8e1ea8ec1f7f7226caae7e4b85e6a7330e6ce0e2
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-05-23 01:07:31 +0300

    Create str_func.py with upper function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_str(value: str):
+    """Функция увеличивает регистр всей строки"""
     return value.upper()
+
+
+def title_str(value: str):
+    """Функция увеличивает регистр первой буквы в каждом слове"""
+    return value.title()
